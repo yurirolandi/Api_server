15f8a07d926149cf5874b2e071e4d216063d80a5
Rolandi <sd.rolandi@gmail.com>
2021-03-28 14:07:36 -0300

    [fix] - disable eslint

--- src/models/User.js
+++ src/models/User.js
@@ -25,6 +25,7 @@ const UserSchema = new Schema({
 
 }, { timestamps: true });
 
+// eslint-disable-next-line func-names
 UserSchema.pre('save', async function (next) {
   const hash = await bcrypt.hash(this.password, 10);
   this.password = hash;
