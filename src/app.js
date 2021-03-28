83c54e7ea9025de8a88501195de0567646ff0cc9
Rolandi <sd.rolandi@gmail.com>
2021-03-28 12:37:14 -0300

    [update] - conecção com o banco

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
   constructor() {
     this.app = express();
+
+    mongoose.connect(
+      'mongodb+srv://rolandi21:86864564rr@cluster0.vhfiu.mongodb.net/Cluster0?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+      },
+    );
+
     this.middlewares();
     this.routes();
   }
