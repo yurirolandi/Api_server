424fdb39221c2f176a4a04047043b65a84e03236
Rolandi <sd.rolandi@gmail.com>
2021-03-28 12:38:23 -0300

    [update] - registro de usuarios model

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const UserSchema = new Schema({
+  nome: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  idade: {
+    type: Number,
+    require: false,
+    unique: false,
+  },
+  password: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+
+}, { timestamps: true });
+
+UserSchema.pre('save', async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+export default model('User', UserSchema);
