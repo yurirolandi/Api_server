62664b2e17c6978c6f39d7cada3fe9abb97faee3
Rolandi <sd.rolandi@gmail.com>
2021-03-28 12:37:30 -0300

    [update] - rota de registro

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ import homeController from './controllers/HomeController';
 const routes = new Router();
 
 routes.get('/', homeController.index);
+routes.post('/registerUser', homeController.createUser);
 
 export default routes;
