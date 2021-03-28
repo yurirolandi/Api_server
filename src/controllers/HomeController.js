728dc9a00c075fa92d4ec04d3291a7eaa1f498df
Rolandi <sd.rolandi@gmail.com>
2021-03-28 12:38:03 -0300

    [update] - registro de usuario controllers

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,49 @@
+import User from '../models/User';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
-    });
+  async index(req, res) {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: 'Usuário inexistente',
+      });
+    }
+
+    return res.json(user);
+  }
+
+  async createUser(req, res) {
+    try {
+      const {
+        email,
+        nome,
+        idade,
+        password,
+      } = req.body;
+
+      const userEmail = await User.findOne({ email });
+      if (userEmail) {
+        return res.status(400).json({
+          message: 'Email já cadastrado!',
+        });
+      }
+
+      const newUser = await User.create({
+        email,
+        nome,
+        idade,
+        password,
+      });
+
+      newUser.password = undefined;
+
+      return res.send(newUser);
+    } catch (error) {
+      console.log('error => ', error);
+      return res.status(400).send({ error: 'Register failed' });
+    }
   }
 }
 
