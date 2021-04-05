86fc10f555141fe54bb9555b625f86267d5690a0
Rolandi <sd.rolandi@gmail.com>
2021-04-05 08:56:40 -0300

    [feat] - rota de colunas

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import Router from 'express';
 import homeController from './controllers/HomeController';
+import GrausController from './controllers/GrausController';
 
 const routes = new Router();
 
 routes.get('/', homeController.index);
 routes.post('/registerUser', homeController.createUser);
 routes.post('/autenticate', homeController.autencicate);
+
+routes.get('/graus', GrausController.index)
+routes.post('/registergraus', GrausController.create)
 
 export default routes;
