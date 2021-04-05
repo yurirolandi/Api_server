016a683cca75552e45ee7fbe131b147f7d9da449
Rolandi <sd.rolandi@gmail.com>
2021-04-05 08:57:22 -0300

    [fix] - autentication secret key

--- src/middlewares/userAutentication.js
+++ src/middlewares/userAutentication.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import authConfig from '../config/auth.json';
+import authConfig from '../config/auth';
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
