cd636c46006dd86e0bdf2df6f8a84c27d9f3931e
Rolandi <sd.rolandi@gmail.com>
2021-04-06 18:22:13 -0300

    [feat] - multer config

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import Router from 'express';
+import multer from 'multer';
 import homeController from './controllers/HomeController';
 import GrausController from './controllers/GrausController';
+import FotoController from './controllers/FotoController';
+import multerConfig from './config/multerConfig';
+
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
 routes.get('/', homeController.index);
-routes.post('/registerUser', homeController.createUser);
-routes.post('/autenticate', homeController.autencicate);
+routes.post('/registerUser/', homeController.createUser);
+routes.post('/autenticate/', homeController.autencicate);
 
 routes.get('/graus', GrausController.index)
-routes.post('/registergraus', GrausController.create)
+routes.post('/registergraus/', GrausController.create)
+
+routes.post('/foto/', upload.single('foto'), FotoController.store)
 
 export default routes;
