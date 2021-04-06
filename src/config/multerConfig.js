bca73988e81a8001b8e23220ce02355679695477
Rolandi <sd.rolandi@gmail.com>
2021-04-06 18:34:48 -0300

    [feat] - filter em arquivos recebidos

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -2,12 +2,18 @@ import multer from 'multer';
 import { extname, resolve } from 'path';
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/jpg') {
+      return cb(new multer.MulterError('Arquivo precisar ser PNG, JPEG OU JPG.'))
+    }
+    return cb(null, true)
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'))
-    }, 
+    },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}${extname(file.originalname)}`)
-    }, 
+    },
   })
 };
