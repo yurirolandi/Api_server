d793663cd1cead5e1a87a2457db8a89aea6c9066
Rolandi <sd.rolandi@gmail.com>
2021-04-06 18:34:26 -0300

    [feat] - clean router

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,10 @@
 import Router from 'express';
-import multer from 'multer';
 import homeController from './controllers/HomeController';
 import GrausController from './controllers/GrausController';
 import FotoController from './controllers/FotoController';
-import multerConfig from './config/multerConfig';
 
-const upload = multer(multerConfig);
+
+
 
 const routes = new Router();
 
@@ -16,6 +15,6 @@ routes.post('/autenticate/', homeController.autencicate);
 routes.get('/graus', GrausController.index)
 routes.post('/registergraus/', GrausController.create)
 
-routes.post('/foto/', upload.single('foto'), FotoController.store)
+routes.post('/foto/', FotoController.store)
 
 export default routes;
